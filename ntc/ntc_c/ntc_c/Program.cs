9689c2fb63f3b42c8c9f7cbd4195c1c5ba76fa7f
Aleksandr Petushkov <aleks.petushkov@gmail.com>
2016-02-24 18:08:13 +0600

    begin

---
+++ ntc/ntc_c/ntc_c/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntc_c
+{
+    static class Program
+    {
+        static UInt16 Main()
+        {
+            Console.re
+
+
+
+            return 0;
+        }
+    }
+}
